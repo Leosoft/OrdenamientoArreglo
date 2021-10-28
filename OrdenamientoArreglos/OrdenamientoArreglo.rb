d2e42740a690436ce9a511a8a83db4bd030d5b1b
Leonardo Zabala <37641186+Leosoft@users.noreply.github.com>
2021-10-27 21:56:33 -0300

    Add files via upload

---
+++ OrdenamientoArreglos/OrdenamientoArreglo.rb
@@ -0,0 +1,12 @@
+
+edades = [14,6,2,18,23,64,13,77]
+
+edades_descendentes = edades.sort
+
+palabras = ["Ruby", "Python", "Java","C"]
+
+palabras_descendentes = palabras.sort
+
+print edades_descendentes
+puts
+print palabras_descendentes
